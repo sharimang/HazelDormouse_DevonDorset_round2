93b9552e22fa94509db85c44f00d5088c898954d
Mang <sm784@rstudio04n.isad.isadroot.ex.ac.uk>
2023-11-03 14:55:06 +0000

    test

--- scripts/test.R
+++ scripts/test.R
@@ -2,3 +2,8 @@
 # blah blah blah
 credentials::set_github_pat()
 
+# git log
+
+# three states - modified, staged, committed 
+# can't commit anything from modified state - has to be staged
+# files in staged state will be committed to github repo next time commit is run
